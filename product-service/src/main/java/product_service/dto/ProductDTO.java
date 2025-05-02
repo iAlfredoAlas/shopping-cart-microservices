683fc2bf843a86b8bc7e95a60d8d3535e05681be
Alfredo Alas <alfredo.alas14@gmail.com>
2025-05-02 06:39:26 -0600

    feat: add ProductDTO.java to map product data from external API

---
+++ product-service/src/main/java/product_service/dto/ProductDTO.java
@@ -0,0 +1,15 @@
+package product_service.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+    private Long id;
+    private String title;
+    private Double price;
+    private String description;
+    private String category;
+    private String image;
+
+}
