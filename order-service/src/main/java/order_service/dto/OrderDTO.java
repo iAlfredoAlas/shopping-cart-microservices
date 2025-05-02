ffb8c3d4e54e7a086c30392f932aa9f4542b9f6b
Alfredo Alas <alfredo.alas14@gmail.com>
2025-05-02 08:34:57 -0600

    feat: add OrderDTO and OrderItemDTO with validation annotations

---
+++ order-service/src/main/java/order_service/dto/OrderDTO.java
@@ -0,0 +1,18 @@
+package order_service.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OrderDTO {
+
+    @NotNull(message = "Customer ID is required")
+    private Long customerId;
+
+    @NotEmpty(message = "Order must contain at least one item")
+    private List<OrderItemDTO> items;
+
+}
